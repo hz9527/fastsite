387e44313431b93b6574040707089cdaa24f9ec6
huangzhong <huangzhong@daojia.com>
2017-12-26 19:17:31 +0800

    test

---
+++ serve/test/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const privateKey = fs.readFileSync('./test.pem', 'utf8')
+const certificate = fs.readFileSync('./certificate.pem', 'utf8')
+const credentials = {key: privateKey, cert: certificate}
+
+const app = express()
+
+app.use(express.static('./public'))
+
+const httpsServer = https.createServer(credentials, app)
+
+httpsServer.listen(8088, function() {
+    console.log('HTTPS Server is running on: https://localhost:%s', 8088);
+})
